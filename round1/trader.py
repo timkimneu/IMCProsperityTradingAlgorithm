be293f20d3355546867d7fe21604146e5c8b2384
DanielYu <thgreatexplorer@gmail.com>
2024-04-10 16:57:40 -0400

    round1 algo

--- round1/trader.py
+++ round1/trader.py
@@ -4,7 +4,27 @@ import string
 import jsonpickle
 import json
 import numpy as np
+from enum import enum
+import math
 
+# constants
+AMETHYSTS = "AMETHYSTS"
+STARFRUIT = "STARFRUIT"
+
+PRODUCTS = [
+    AMETHYSTS,
+   STARFRUIT,
+]
+
+DEFAULT_PRICES = {
+    AMETHYSTS : 10_000,
+    STARFRUIT : 5_000, # starfruit price will change
+}
+
+class STRATEGY_TYPE(enum):
+    NAIVE = 0
+    ROBUST = 1
+    COMPLEX = 2
 
 class Logger:
     def __init__(self) -> None:
@@ -118,8 +138,252 @@ class Trader:
         """
         Intializes the Trader class
         """
-        self.price_history = []  # To store the last 30 timesteps of prices
+        self.rolling_window = 30 # set the rolling window as 30
+
+        self.price_history = dict()  # To store the last self.rolling_window timesteps of price data
+        for product in PRODUCTS:
+            self.price_history[product] = []
+
+        self.ema_prices = dict() # To store the predicted EMA prices 
+        for product in PRODUCTS:
+            self.ema_prices[product] = None
+
         self.position_limit = 20 # set the position limit as 20 for the tutorial/round1 TODO Remember to change this for each round
+        self.cash = 0 # start with 0 cash
+        self.amethyst_strategy = STRATEGY_TYPE.NAIVE # test different strategies
+        self.starfruit_strategy = STRATEGY_TYPE.NAIVE # test different strategies
+        self.vol_threshold = 1.5 # our volatility strategy
+        
+
+    def get_mid_price(self, product, state :TradingState) -> float:
+        """Calculates the mid_price based on the average of the lowest ask and highest bid"""
+        order_depth: OrderDepth = state.order_depths[product]
+
+        market_bids = order_depth.buy_orders
+        if len(market_bids) == 0:
+            # There are no bid orders in the market (midprice undefined)
+            return DEFAULT_PRICES[product]
+        
+        market_asks = order_depth.sell_orders
+        if len(market_asks) == 0:
+            # There are no ask orders in the market (mid_price undefined)
+            return DEFAULT_PRICES[product]
+        
+        best_bid = max(market_bids)
+        best_ask = min(market_asks)
+        logger.print(str((best_bid + best_ask)/2))
+        return (best_bid + best_ask)/2
+    
+    def get_position(self, product, state :TradingState) -> int:
+        """Gets the current position size of a product. Returns 0 if no position."""
+        return state.position.get(product, 0)
+    
+    def get_value_on_product(self, product, state : TradingState):
+        """
+        Returns the amount of MONEY currently held on the product.  
+        """
+        return self.get_position(product, state) * self.get_mid_price(product, state)
+    
+    #TODO
+    def update_pnl(self, state :TradingState) -> None:
+        #TODO
+        return 
+
+    def calculate_volume(self, product, state :TradingState) -> tuple[int,int]:
+        """
+        Calculates the total volume of buy and sell orders for this product in the current state of the orderbook. Returns (buy volume, sell volume) where sell volume is positive.
+        """
+        order_depth: OrderDepth = state.order_depths[product]
+
+        # buy volume
+        buy_orders = list(order_depth.buy_orders.items())
+        buy_vol = 0
+        for idx in range(0,len(sell_orders)):
+            bid_price, bid_amount = buy_orders[idx]
+            buy_vol += bid_amount
+
+        # sell volume
+        sell_orders = list(order_depth.sell_orders.items())
+        sell_vol = 0
+        for idx in range(0,len(sell_orders)):
+            ask_price, ask_amount = sell_orders[idx]
+            sell_vol += ask_amount
+
+        return (buy_vol,sell_vol)
+    
+    def filter_orderbook(self,product,price:float,state:TradingState,buy_order:bool) -> dict[int,int]:
+        """Filters all the orders that are above/below the price depending on if they are sell or buy orders."""
+        order_depth: OrderDepth = state.order_depths[product]
+
+        filtered_order_book = {}
+
+        if buy_order:
+            buy_orders = order_depth.buy_orders.items()
+            for idx in range(0,len(buy_orders)):
+                bid_price, bid_amount = buy_orders[idx]
+                if bid_price >= price:
+                    filtered_order_book.update(buy_orders[idx])
+        else:
+            sell_orders = order_depth.sell_orders.items()
+            for idx in range(0,len(sell_orders)):
+                ask_price, ask_amount = sell_orders[idx]
+                if ask_price <= price:
+                    filtered_order_book.update(sell_orders[idx])
+
+        return filtered_order_book
+    
+    def fill_orders(order_book :dict[int,int], product) -> List[Order]:
+        """
+        Fills a dictionary of market orders in hopes of executing a trade.
+
+        - order_book: dictionary of {price, quantity (+/-)} pairs 
+        - product: the product
+
+        Returns a List[Order(price, symbol, quantity)]
+        """
+        return
+    
+    def calculate_volatility(self, product) -> float:
+        """
+        Calculates the volatility of a product from its price data over the rolling period of self.rolling_window
+        - #TODO use the returns volatility?
+        """
+        price_history = np.array(self.price_history[product])
+        #returns = np.diff(price_history) / price_history[:-1] 
+        std_dev = np.std(price_history)
+        return std_dev
+    
+    def calculate_position_sizing(self, product, state :TradingState) -> int:
+        """Computes position sizing for a given product based on volatility"""
+        return 
+    
+    def update_ema_prices(self, product, state :TradingState) -> None:
+        """
+        Update the exponential moving average of the prices of each product.
+        """
+        for product in PRODUCTS:
+            mid_price = self.get_mid_price(product, state)
+
+            # if the ema is not calculated yet (before rolling period has started), then set the ema to the mid_price
+            if self.ema_prices[product] == None:
+                self.ema_prices[product] = mid_price
+            else:
+                ema_param = (2 / (self.rolling_window + 1)) # calculate the ema_param #TODO could mess with this value
+                self.ema_prices[product] = ema_param * mid_price + (1-ema_param) * self.ema_prices[product]
+        return
+    
+    def amethyst_naive_strategy(self, state :TradingState) -> List[Order]:
+        position_amethysts = self.get_position(AMETHYSTS, state)
+
+        bid_volume = self.position_limit - position_amethysts 
+        ask_volume = - self.position_limit - position_amethysts 
+
+        orders = []
+        orders.append(Order(AMETHYSTS, DEFAULT_PRICES[AMETHYSTS] - 2, bid_volume))
+        orders.append(Order(AMETHYSTS, DEFAULT_PRICES[AMETHYSTS] + 2, ask_volume))
+        return orders
+
+    def amethyst_strategy(self, state :TradingState) -> None:
+        """
+        Market make around 10000. 
+        - Naive Strategy is to post max order sizes at sell price +2 and buy price -2
+        - Robust Strategy is to buy/sell +2 and -2 and calculating posiiton size based on volume/current market orders
+        """
+
+        if self.amethyst_strategy == STRATEGY_TYPE.NAIVE:
+            orders = self.amethyst_naive_strategy(state)
+        elif self.amethyst_strategy == STRATEGY_TYPE.ROBUST:
+            #TODO
+            print()
+        elif self.amethyst_strategy == STRATEGY_TYPE.COMPLEX:
+            # TODO 
+            print()
+        return orders 
+    
+    def starfruit_naive_strategy(self, state :TradingState):
+
+        def calculate_linear_reg(self, product):
+            """Runs a least squares linear regression through the price data in the self.rolling_windows timeframe"""
+            y_vals = np.array(self.price_history[product])
+            x_vals = np.arange(0,self.rolling_window)
+            x_mean = np.mean(x_vals)
+            y_mean = np.mean(y_vals)
+            slope = np.sum((x_vals - x_mean) * (y_vals - y_mean)) / np.sum((x_vals - x_mean)**2)
+            b = y_mean - slope * x_mean
+
+            return slope, b
+
+        volatility = self.calculate_volatility(STARFRUIT)
+
+        position_starfruit = self.get_position(STARFRUIT, state)
+
+        bid_volume = self.position_limit - position_starfruit
+        ask_volume = - self.position_limit - position_starfruit
+
+
+        if volatility <= self.vol_threshold:
+            orders = []
+
+            # market make around the ema
+            orders.append(Order(STARFRUIT, math.floor(self.ema_prices[STARFRUIT] - 1), bid_volume))
+            orders.append(Order(STARFRUIT, math.ceil(self.ema_prices[STARFRUIT] + 1), ask_volume))
+            
+            return orders
+        
+        else: # if the volatility is high, then predict the price to increase/decrease directionally following a linear trend
+
+            slope, _ = calculate_linear_reg(STARFRUIT)
+            position_starfruit = self.get_position(STARFRUIT, state)
+
+            if slope > 0:
+                # we predict the mid price will continue to go up and that there is likely more mispricing
+                # we want to hold an overall long position. This means that in expectation that the price will go up, we place a bid
+                # slightly higher than expected. 
+                orders.append(Order(STARFRUIT, math.ceil(self.ema_prices[STARFRUIT]), bid_volume))
+                # we want to sell at a higher price, so we set the ask price higher
+                orders.append(Order(STARFRUIT, math.floor(self.ema_prices[STARFRUIT] + 2), ask_volume))
+
+            else:
+                # we predict that the mid price will continue to go down and that there is likely more mispricing
+                # we want to hold an overall short position. We will place a bid slightly lower than expected at a 1/2 the volume
+                orders.append(Order(STARFRUIT, math.floor(self.ema_prices[STARFRUIT] - 2), math.floor(bid_volume / 2)))
+                # we want to liquidate our position until the trend reverses, so our ask volume will still be the same.
+                orders.append(Order(STARFRUIT, math.ceil(self.ema_prices[STARFRUIT]), ask_volume))
+
+    
+    def starfruit_strategy(self, state :TradingState) -> None:
+        """
+        Trade based on the mid_price estimate of an exponential moving average of the rolling period. 
+        - Naive Strategy is buy/sell to the max position limit at the ema_price + 1/-1 unless the volatility of prices is above +1.5/-1.5 (which suggests price corrections). At which point we then trade on the linear trend
+
+        """
+        if self.starfruit_strategy == STRATEGY_TYPE.NAIVE:
+            orders = self.starfruit_naive_strategy(state)
+        elif self.starfruit_strategy == STRATEGY_TYPE.ROBUST:
+            #TODO
+            print()
+        elif self.starfruit_strategy == STRATEGY_TYPE.COMPLEX:
+            # TODO 
+            print()
+        return orders
+    
+    def serialize_state(self, state: TradingState) -> str:
+        # Deserialize the traderData from the last iteration
+        if state.traderData and state.traderData.strip():
+            self.price_history = jsonpickle.decode(state.traderData)
+        else:
+            raise ValueError("No TraderData")
+            
+        for product in PRODUCTS:
+            # Update the price history with new data from the current iteration
+            mid_price = self.get_mid_price(product, state)
+            self.price_history[product].append(mid_price)
+            if len(self.price_history[product]) > self.rolling_window:
+                # Keep only the last 30 days of prices, for example
+                self.price_history[product] = self.price_history[product][-self.rolling_window:]
+
+        # Serialize the updated price history for the next iteration
+        return jsonpickle.encode(self.price_history)
 
     def run(self, state: TradingState):
         # Only method required. It takes all buy and sell orders for all symbols as an input,
@@ -132,84 +396,52 @@ class Trader:
             orders: List[Order] = []
 
             logger.print(product)
-            logger.print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", " + str(order_depth.buy_orders) + ", Sell order depth : " + str(len(order_depth.sell_orders)) + ", " + str(order_depth.buy_orders))
+            logger.print("Buy Order depth : " + str(len(order_depth.buy_orders)) + ", " + str(order_depth.buy_orders) + ", Sell order depth : " + str(len(order_depth.sell_orders)) + ", " + str(order_depth.sell_orders))
             logger.print("Own Trades: " + str(state.own_trades))
             logger.print("Market Trades: " + str(state.market_trades))
             logger.print("Own Positions: " + str(state.position))
             logger.print()
 
             if product == "AMETHYSTS":
-                acceptable_price = 10000 # market make around 10k. #TODO position sizing
+                acceptable_price = PRODUCTS[0] # market make around 10k. 
+                logger.print("Acceptable price : " + acceptable_price)
+                orders = self.amethyst_strategy(state) #TODO position sizing for other strategies
+
             elif product == "STARFRUIT":
-                if len(self.price_history) >= 30:
-                    price_history = np.array(self.price_history) # Participant should calculate this value
-                    acceptable_price = np.average(price_history) # mean-reversion strategy
-                    # implement stop-loss/take-profit and standard deviation or % drift
-                else:
-                    acceptable_price = 5000 # set to default of 5000
-
-            logger.print("Acceptable price : " + str(acceptable_price))
-
-            if len(order_depth.sell_orders) != 0:
-                best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
-                #TODO fix the position size checks
-                # check if the amount would go over the position limit, if yes, then fill up to position limit
-                #if abs(-best_ask_amount + state.position[product]) > self.position_limit:
-                #    best_ask_amount = self.position_limit - abs(state.position[product])
-                if int(best_ask) < acceptable_price:
-                    logger.print("BUY", str(-best_ask_amount) + "x", best_ask)
-                    orders.append(Order(product, best_ask, -best_ask_amount))
-
-            if len(order_depth.buy_orders) != 0:
-                best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
-                #TODO fix the position size checks
-                # check if the amount would go over the position limit, if yes, then fill up to position limit
-                #if abs(best_bid_amount + state.position[product]) > self.position_limit:
-                #    best_bid_amount = self.position_limit - abs(state.position[product])
-                if int(best_bid) > acceptable_price:
-                    logger.print("SELL", str(best_bid_amount) + "x", best_bid)
-                    orders.append(Order(product, best_bid, -best_bid_amount))
+                 if len(self.price_history) >= self.rolling_window:
+                    logger.print("Acceptable price : " + str(self.ema_prices))
+                    orders = self.starfruit_strategy(state) #TODO position sizing for other strategies
 
             logger.print("Own Orders: " + str(orders))
 
             result[product] = orders
             
-            # if it is starfruit
-            if product == "STARFRUIT":
-                # calculate the mid_price
-                price = (best_ask + best_bid) / 2
-        
         # serialize the previous mid_prices for STARFRUIT and store those values
-        traderData = self.serialize_state(state,price) # String value holding Trader state data required.
+        traderData = self.serialize_state(state) # String value holding Trader state data required.
         # It will be delivered as TradingState.traderData on next execution.
 
-        conversions = 1
+        conversions = 1 # not needed for round1
         logger.flush(state, result, conversions, traderData)
         return result, conversions, traderData
 
-    def serialize_state(self, state: TradingState, curr_price :int) -> str:
-        # Deserialize the traderData from the last iteration
-        if state.traderData and state.traderData.strip():
-            self.price_history = jsonpickle.decode(state.traderData)
-        else:
-            self.price_history = []
+"""
+ if len(order_depth.sell_orders) != 0:
+    best_ask, best_ask_amount = list(order_depth.sell_orders.items())[0]
+    #TODO fix the position size checks
+    # check if the amount would go over the position limit, if yes, then fill up to position limit
+    #if abs(-best_ask_amount + state.position[product]) > self.position_limit:
+    #    best_ask_amount = self.position_limit - abs(state.position[product])
+    if int(best_ask) < acceptable_price:
+        logger.print("BUY", str(-best_ask_amount) + "x", best_ask)
+        orders.append(Order(product, best_ask, -best_ask_amount))
 
-        # Update the price history with new data from the current iteration
-        # Assuming you have a method to fetch or calculate the current price
-        self.price_history.append(curr_price)
-        if len(self.price_history) > 30:
-            # Keep only the last 30 days of prices, for example
-            self.price_history = self.price_history[-30:]
-
-        # Serialize the updated price history for the next iteration
-        return jsonpickle.encode(self.price_history)
-    
-# Trading Strategies
-# def moving_avg(timeframe :int, state: TradingState) -> int:
-        """
-        Calculates the moving average based on the the past x days.
-        - timeframe : past x days
-        - state : current trading state
-
-        returns the predicted price
-        """
+if len(order_depth.buy_orders) != 0:
+    best_bid, best_bid_amount = list(order_depth.buy_orders.items())[0]
+    #TODO fix the position size checks
+    # check if the amount would go over the position limit, if yes, then fill up to position limit
+    #if abs(best_bid_amount + state.position[product]) > self.position_limit:
+    #    best_bid_amount = self.position_limit - abs(state.position[product])
+    if int(best_bid) > acceptable_price:
+        logger.print("SELL", str(best_bid_amount) + "x", best_bid)
+        orders.append(Order(product, best_bid, -best_bid_amount))
+"""
